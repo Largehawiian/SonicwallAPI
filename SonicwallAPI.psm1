235fcfd68e3a8f1e1ed04c7e1bd0d1a41cfb8fac
Largehawiian <jeremyorama@gmail.com>
2020-02-07 17:12:38 -0500

    Update

--- SonicwallAPI.psm1
+++ SonicwallAPI.psm1
@@ -1,7 +1,7 @@
 # Importing public and private functions
-$Bob ="C:\Program Files\WindowsPowerShell\Modules\PSSonicWall\1.0"
-$PublicFunc = @(Get-ChildItem -Path $Bob\Public\*.ps1 -ErrorAction SilentlyContinue)
-$PrivateFunc = @(Get-ChildItem -Path $Bob\Private\*.ps1 -ErrorAction SilentlyContinue)
+$PSScript = "C:\Program Files\WindowsPowerShell\Modules\SonicwallAPI\1.2.0.0"
+$PublicFunc = @(Get-ChildItem -Path $PSScript\Public\*.ps1 -ErrorAction SilentlyContinue)
+$PrivateFunc = @(Get-ChildItem -Path $PSScript\Private\*.ps1 -ErrorAction SilentlyContinue)
 
 # Dotsourcing files
 ForEach ($import in @($PublicFunc + $PrivateFunc)) {
